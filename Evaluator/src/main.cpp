7296cc75afef0a42f272e682c2c5b85b69980ba1
Fadi Farag <fadi50045000@gmail.com>
2021-08-18 23:54:41 -0400

    Handle Parentheses

--- Evaluator/src/main.cpp
+++ Evaluator/src/main.cpp
@@ -50,6 +50,53 @@ double polynomialEvaluator(vector<double> a){
     return currentValue;
 }
 
+int getOperationFromChar(char c){
+	switch(c){
+		case '+':
+			return add;
+		case '-':
+			return subtract;
+		case '*':
+			return multiply;
+		case '/':
+			return divide;
+		case '^':
+			return raise;
+	}
+	
+	return -1;
+}
+
+void simpleParser(string& s, smatch m) {
+	int split_on = m.position();
+	int operation = getOperationFromChar(m[3].str()[0]);
+
+	double firstNumber = atof(m[1].str().c_str());
+	double secondNumber = atof(m[4].str().c_str());
+	
+	double num = polynomialEvaluator({firstNumber, operation*1.0, secondNumber});
+
+	s = s.substr(0, split_on) + to_string(num) + s.substr(split_on + m.length());
+}
+
+double advancedParser(string s){
+	regex parantheses = regex("\\(([^()]+)\\)");
+    regex multDivide = regex("([1-9]\\d*(\\.\\d+)?)(\\*|\\/)([1-9]\\d*(\\.\\d+)?)");
+    regex addSubtract = regex("([1-9]\\d*(\\.\\d+)?)(\\+|\\-)([1-9]\\d*(\\.\\d+)?)");
+	smatch m;
+
+	while(regex_search(s, m, parantheses)){
+		int split_on = m.position();
+
+		s = s.substr(0, split_on) + to_string(advancedParser(m[1].str())) +
+		s.substr(split_on + m.length());
+	}
+	while(regex_search(s, m, multDivide)) simpleParser(s, m);
+	while(regex_search(s, m, addSubtract)) simpleParser(s, m);
+
+    return atof(s.c_str());
+}
+
 // \(([^()]+)\)
 int main(){
     vector<double> a {8, divide, 7, add, 5, subtract, 4};
